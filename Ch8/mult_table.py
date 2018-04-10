c2e07f48e49c47d2faf80686e2ab0bb2292ca134
kprahman <narmak.namhar@gmail.com>
2018-04-10 10:49:02 -0400

    Update mult_table.py
    added context

--- Ch8/mult_table.py
+++ Ch8/mult_table.py
@@ -1,18 +1,21 @@
 def print_range(n):
+    """prints a table header"""
     print("   ", end="\t")
     for i in range(1,n+1):
         print(i, end="\t")
     print("\n :---"+("--")*((2*n)-1))
 
-def mult_table(n,m):
+def multiples(n,m):
+    """prints m multiples of m"""
     print(n,end=":"+"\t")
     for i in range(1,m):
         print(n*i, end="\t")
     print()
 
-def styled_mult_table(m):
+def styled_mult_table(m)
+    """prints the header along with a table showing m multiples of m
         print_range(m)
         for i in range(1,m+1):
-            mult_table(i,m+1)
+            multiples(i,m+1)
 
 styled_mult_table(13)
