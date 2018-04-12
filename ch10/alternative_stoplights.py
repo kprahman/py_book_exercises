e483a949bfa05a238253bf5be5617f22a5bf9b93
kprahman <narmak.namhar@gmail.com>
2018-04-12 12:12:12 -0400

    completed timed stoplight. stoplight uses turtles to cycle between green, red, and yellow states.

--- ch10/alternative_stoplights.py
+++ ch10/alternative_stoplights.py
@@ -9,9 +9,23 @@ gerry = turtle.Turtle()
 rory = turtle.Turtle()
 yang = turtle.Turtle()
 turtles = [gerry,rory,yang]
+off= "gray19"
+
+def hideturtles():
+    tess.hideturtle()
+    rory.hideturtle()
+    gerry.hideturtle()
+    yang.hideturtle()
+
+def showturtles():
+    tess.showturtle()
+    gerry.showturtle()
+    rory.showturtle()
+    yang.showturtle()
 
 def draw_housing():
     """Draws a housing for the traffic lights"""
+    tess.speed(900)
     tess.pensize(3)
     tess.color("black", "darkgrey")
     tess.begin_fill()
@@ -23,25 +37,45 @@ def draw_housing():
     tess.left(90)
     tess.end_fill()
 
-def drawlight(t):
+def init_drawlight(t):
     for i in t:
+        i.penup()
         i.shape("circle")
         i.shapesize(3)
+        i.fillcolor(off)
 
+def advance_state_machine():
+    global state_num
+    if state_num == 0: #transition from state 0 to 1
+        gerry.fillcolor("green")
+        yang.fillcolor(off), rory.fillcolor(off)
+        state_num = 1
+        wn.ontimer(advance_state_machine,2000)
+    elif state_num == 1: #transition from 1 to 2
+        yang.fillcolor("yellow")
+        gerry.fillcolor(off), rory.fillcolor(off)
+        state_num = 2
+        wn.ontimer(advance_state_machine, 500)
+    else: #2 to 0
+        rory.fillcolor("red")
+        yang.fillcolor(off), gerry.fillcolor(off)
+
+        state_num = 0
+        wn.ontimer(advance_state_machine, 1000)
+
+hideturtles()
 draw_housing()
+init_drawlight(turtles)
 gerry.setposition(40,50)
 yang.setposition(40,125)
 rory.setposition(40,200)
-drawlight(turtles)
+showturtles()
 
 
+state_num = 0 #holds tess' current state
+advance_state_machine()
 
 
-
-#initialize positions for the stoplights, while hidden. each turtle is 50 units away from one another
-#gerry.hideturtle()
-#rory.hideturtle()
-#yang.hideturtle()
 
 
 
