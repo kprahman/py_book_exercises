49b7f0ab7f155bd57956e2fb02af5b4756f2e4f3
kprahman <narmak.namhar@gmail.com>
2018-04-16 16:03:28 -0400

    Ch11 exercises in progress

---
+++ ch11/text replacer.py
@@ -0,0 +1,14 @@
+def replace(s, old, new):
+    w = s.split(old)
+
+    list_of_words = []
+    for (i,v) in enumerate(w):
+        if i < (len(w)-1):
+            split_word = list(v)
+            split_word.append(new)
+            complete_word = "".join(split_word)
+            list_of_words.append(complete_word)
+        else:
+            list_of_words.append(v)
+    final_string = "".join(list_of_words)
+    return final_string
