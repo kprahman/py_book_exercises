936ea044b2e5891d63dddc7a123b060eaf9380a2
kprahman <narmak.namhar@gmail.com>
2018-04-24 16:59:22 -0400

    ch 14 unorganized/undocumented exercises

--- ch14/load words.py
+++ ch14/load words.py
@@ -78,7 +78,40 @@ def search_binary(xs, target):
         else:
             ub = mid_index        # Use lower half of ROI next time
 
+def find_unknowns_merge_pattern(vocab, wds):
+    """ Both the vocab and wds must be sorted.  Return a new
+        list of words from wds that do not occur in vocab.
+    """
+
+    result = []
+    xi = 0
+    yi = 0
+
+    while True:
+        if xi >= len(vocab):
+            result.extend(wds[yi:])
+            return result
+
+        if yi >= len(wds):
+            return result
+
+        if vocab[xi] == wds[yi]:  # Good, word exists in vocab
+            yi += 1
+
+        elif vocab[xi] < wds[yi]: # Move past this vocab word,
+            xi += 1
+
+        else:                     # Got word that is not in vocab
+            result.append(wds[yi])
+            yi += 1
 
 import time
 
-search_binary(bigger_vocab,"beenis")
+all_words = get_words_in_book("AliceInWonderland.txt")
+t0 = time.clock()
+all_words.sort()
+book_words = remove_adjacent_dups(all_words)
+missing_words = find_unknowns_merge_pattern(bigger_vocab, book_words)
+t1 = time.clock()
+print("There are {0} unknown words.".format(len(missing_words)))
+print("That took {0:.4f} seconds.".format(t1-t0))
